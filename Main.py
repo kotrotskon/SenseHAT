7dbc1787e0de27c0746b510fab4e37f48032af92
kotrotskon <kkotrotsios@gmail.com>
2018-10-04 00:25:40 +0300

    start project

--- Main.py
+++ Main.py
@@ -1,3 +1,7 @@
 from sense_hat import SenseHat
 sense = SenseHat()
-sense.show_message("Elsaki I Love you!!!")
+
+blue = (0, 0, 255)
+yellow = (0, 255, 255)
+
+sense.show_message("Elsaki I Love you!!!", text_color=yellow, back_color=blue)
