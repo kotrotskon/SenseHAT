c8192ce842e9a4b83d0f468e3ffcad399b9aeef8
kotrotskon <kkotrotsios@gmail.com>
2018-10-04 00:27:42 +0300

    start project

--- Main.py
+++ Main.py
@@ -4,4 +4,4 @@ sense = SenseHat()
 blue = (0, 0, 255)
 yellow = (0, 255, 255)
 
-sense.show_message("Elsaki I Love you!!!", text_color=yellow, back_color=blue)
+sense.show_message("Elsaki I Love you!!!", text_colour=yellow, back_colour=blue)
