e3e7c39db09105f8dfd2ce2ed05b4ca2bf6f2d3a
kotrotskon <kkotrotsios@gmail.com>
2018-11-18 16:11:24 +0200

    remove colors

--- Main.py
+++ Main.py
@@ -4,4 +4,4 @@ sense = SenseHat()
 blue = (255, 0, 255)
 yellow = (0, 255, 255)
 
-sense.show_message("Elsaki I Love you!!!", text_colour=yellow, back_colour=blue)
+sense.show_message("Elsaki I Love you!!!")
