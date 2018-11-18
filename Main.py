68fa15cdbf99b68514950e1452c41c3a084b361f
kotrotskon <kkotrotsios@gmail.com>
2018-11-18 16:24:48 +0200

    add temp, press, humidity

--- Main.py
+++ Main.py
@@ -3,10 +3,20 @@ sense = SenseHat()
 
 sense.clear()
 
-pressure = sense.get_pressure()
-print("pressure: ", pressure)
+while True:
+    # Take readings from all three sensors
+    t = sense.get_temperature()
+    p = sense.get_pressure()
+    h = sense.get_humidity()
 
-temp = sense.get_temperature()
-print("temperature: ", temp)
+    # Round the values to one decimal place
+    t = round(t, 1)
+    p = round(p, 1)
+    h = round(h, 1)
 
+    # Create the message
+    # str() converts the value to a string so it can be concatenated
+    message = "Temperature: " + str(t) + " Pressure: " + str(p) + " Humidity: " + str(h)
 
+    # Display the scrolling message
+    sense.show_message(message, scroll_speed=0.05)
